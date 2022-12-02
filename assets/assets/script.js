26c352634a850b56c4af9ef0ab5cc4c82b6fcc54
DanielkKostich <117228151+DanielkKostich@users.noreply.github.com>
2022-12-02 16:22:38 -0600

    added Javascript functions

--- assets/assets/script.js
+++ assets/assets/script.js
@@ -1,19 +1,27 @@
-var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
+var Topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
+var randomTopic = Topics[Math.floor(Math.random() * Topics.length)];
 
-for (var x = 0; x < topics.length; x++) {
-    console.log(topics[x]);
+
+function ListTopics() {
+ for (var x = 0; x < Topics.length; x++) {
+    console.log(Topics[x]);
+ }
 }
 
-/*
-if(topic === 'HTML') {
+function selectTopic() {
+ if (randomTopic === 'HTML') {
     console.log("Lets,s study HTML!");
-} else if (topic === 'CSS') {
+} else if (randomTopic === 'CSS') {
     console.log("Let,s study CSS!");
-} else if (topic === 'Git') {
+} else if (randomTopic === 'Git') {
     console.log("Let,s study Git!");
-} else if (topic === 'Javascript') {
-    console.log("Let,s study Javascript!") ;
+} else if (randomTopic === 'Javascript') {
+    console.log("Let,s study Javascript!");
 } else {
     console.log("Please try again!")
+ }
 }
-*/
+console.log('Here are the topics we learned through Prework:');
+ListTopics();
+console.log('Which topic should we study first?');
+selectTopic()
